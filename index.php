bee80ddaca3d1845e6370c5d46c26b3a81d3fd6c
david <david@webrelief.fr>
2024-02-14 14:01:40 +0100

    ajout de the_content dans l'index

--- index.php
+++ index.php
@@ -7,8 +7,9 @@ echo ('<h1>PROJET CAPEB</h1>')
 <?php 
 echo ('<h2>PROJET CAPEB</h2>');
 echo ('<h3>PROJET CAPEB</h3>');
-echo ('<h5>PROJET CAPEB</h5>')
+echo ('<h5>PROJET CAPEB</h5>');
 
+echo the_content();
 ?>
 
 <?php get_footer();?>
