7759501bad80fe5ade947d6ea83466ed6d498a3a
Gregory Najm <gregory.najm@ipag.fr>
2024-02-14 13:54:31 +0100

    test

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ echo ('<h1>PROJET CAPEB</h1>')
 
 <?php 
 echo ('<h2>PROJET CAPEB</h2>');
-echo ('<h3>PROJET CAPEB</h3>')
+echo ('<h3>PROJET CAPEB</h3>');
+echo ('<h4>PROJET CAPEB</h4>')
+
 ?>
 
 <?php get_footer();?>
