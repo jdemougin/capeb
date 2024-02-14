07b078366b2a41509b139a2cb4f64f022e67c764
david <david@webrelief.fr>
2024-02-14 14:31:31 +0100

    h6 à 4

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php get_header();?>
 
 <?php 
-echo ('<h1>PROJET CAPEB</h1>')
+echo ('<h6>PROJET CAPEB</h6>')
 ?>
 
 <?php 
-echo ('<h2>PROJET CAPEB</h2>');
-echo ('<h3>PROJET CAPEB</h3>');
-echo ('<h4>PROJET CAPEB</h4>');
+echo ('<h5>PROJET CAPEB</h2>');
+echo ('<h4>PROJET CAPEB</h3>');
+echo ('<h3>PROJET CAPEB</h4>');
 echo ('<h5>MERGE MERGE MERGE</h5>');
 
 
